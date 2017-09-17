301051091df5da640f9af988b5e1594a94b8318a
rgx6 <rgx@hotmail.co.jp>
2017-09-18 05:52:42 +0900

    追加した画像をアニメーションのリストに追加。

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var gulp   = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
+gulp.task('default', ['js', 'image']);
+
+gulp.task('js', function () {
     gulp.src('src/*.js')
         .pipe(gulp.dest('dest'))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('dest'));
+});
+
+gulp.task('image', function () {
+    gulp.src('src/image/*')
+        .pipe(gulp.dest('dest/image'));
 });
