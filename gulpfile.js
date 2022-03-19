3a190527e7dcace5c9b4d9db4302b063cb2521ea
rgx6 <rgx@hotmail.co.jp>
2022-03-19 18:32:59 +0900

    packageのバージョンアップ

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,19 @@ var gulp   = require('gulp');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['js', 'image']);
-
-gulp.task('js', function () {
+gulp.task('js', function (done) {
     gulp.src('src/*.js')
         .pipe(gulp.dest('dest'))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('dest'));
+    done();
 });
 
-gulp.task('image', function () {
+gulp.task('image', function (done) {
     gulp.src('src/image/*')
         .pipe(gulp.dest('dest/image'));
+    done();
 });
+
+gulp.task('default', gulp.series('js', 'image'));
